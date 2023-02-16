5f3b80edf494bb07707269b8b354572b8d90e4c3
Lechu <alsele.floridauniversitaria.es>
2023-02-16 10:13:52 +0100

    primer commit

--- atotal.sh
+++ atotal.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+#se intenta elegir la ciudad específicada con el Grep, mientras que con Consumos se intenta agarrar toda la columna de consumos
 read -p "qué ciudad Eliges?" consumovariable 
-$ciudad cat consumos.txt | awk "NR == 1 { print $1}" consumos.txt
+$ciudad cat consumos.txt | grep "Valencia" && grep "Barcelona" && grep "Madrid"  consumos.txt
 $consumos awk 'NR == 4 { print $4}' consumos.txt  
 if [[ $consumovariable==$ciudad ]]
  echo "estos son lo que consume $consumos"
